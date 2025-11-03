22373ee6feb7f3e352a7619febb1bb6fc556fe87
DoniaAli77 <donia.ali.7799@gmail.com>
2025-11-03 13:14:03 +0200

    fix v8 subpage

--- frontend/app/parallel-routes/@students/page.tsx
+++ frontend/app/parallel-routes/@students/page.tsx
@@ -11,7 +11,7 @@ export default function StudentsSlot() {
       <Students /> 
       
       <div className="flex flex-col items-center min-h-screen bg-[#121212] p-6">
-      <h1 className="text-3xl font-bold text-white mb-8"><Link href="/enrollments/subpage">CLICK  Sub page</Link>  </h1>
+      <h1 className="text-3xl font-bold text-white mb-8"><Link href="/parallel-routes/subpage">CLICK  Sub page</Link>  </h1>
       </div>
       </>
       
--- frontend/app/parallel-routes/@students/subpage/page.tsx
+++ frontend/app/parallel-routes/@students/subpage/page.tsx
@@ -6,7 +6,7 @@ export default function Subpage() {
     <div className="flex flex-col items-center min-h-screen bg-[#121212] p-6">
       <h1 className="text-3xl font-bold text-white mb-8">hello from sub page  </h1>
       
-      <h2 className="text-3xl font-bold text-white mb-8">  <Link href="/enrollments"> CLICK  back to students list</Link></h2>
+      <h2 className="text-3xl font-bold text-white mb-8">  <Link href="/parallel-routes"> CLICK  back to students list</Link></h2>
     
     </div>
   );
